784ed905bfa47e15049d8c9f68541a5e93e0a418
Juliette_17 <julita.oltusek@gmail.com>
2020-01-18 19:27:34 +0100

    modify approximated function and dataset

--- src/data_generator.py
+++ src/data_generator.py
@@ -5,12 +5,12 @@ DATASET_SIZE = 10000
 DENSITY = 0.1
 
 def load_dataset():
-    dataset = pd.read_csv('dataset.csv')
+    dataset = pd.read_csv('dataset2.csv')
     return dataset
 
 def save_dataset():
     df = prepare_dataset(DATASET_SIZE)
-    df.to_csv('dataset.csv', index = False)
+    df.to_csv('dataset2.csv', index = False)
 
 def prepare_dataset(set_size):
     points = generate_points(set_size)
@@ -20,7 +20,7 @@ def prepare_dataset(set_size):
     return data
 
 def generate_points(points_number=DATASET_SIZE, density=DENSITY):
-    # f(x,y) = sin(x)*cos(5*y)/5
+    # f(x,y) = 5*sin(0.1*x)*cos(0.5*y)
 
     # generate range of area to generate points, x and y both in range [-sphere_range, sphere_range)
     sphere_range = int(sqrt(points_number)/2)
@@ -30,7 +30,7 @@ def generate_points(points_number=DATASET_SIZE, density=DENSITY):
     points = []
     for x in range(xmin, xmax, 1):
         for y in range(ymin, ymax, 1):
-            z = sin(x*density)*cos(5*y*density)/5 # calculate z coordinate with given f(x,y)
+            z = 5*sin(0.1*x*density)*cos(0.5*y*density) # calculate z coordinate with given f(x,y)
             points.append([x*density,y*density,z]) 
 
     return points
