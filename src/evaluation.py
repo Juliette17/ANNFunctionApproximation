47d90f0bdd9f4cfc298c9a074ff224813acc75b7
Rafał Koguciuk <rafalkoguciuk33@gmail.com>
2020-01-26 01:20:44 +0100

    ploting surface

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,4 +1,5 @@
 from model import get_datasets, train_model, build_model, prepare_dataset
+from data_generator import plot_surface
 
 import pickle
 from keras.models import load_model
@@ -32,6 +33,7 @@ def run(train_new_model=False, model_file='model.h5', history_file='trainHistory
 
     return model, history
 
+
 def evaluate_manually(X_train, y_train, X_valid, y_valid, model, history):
     # X_test, y_test - to evaluate model only at the end of project!!!!!!!
     train_results = model.evaluate(X_train, y_train)
@@ -46,19 +48,14 @@ def evaluate_manually(X_train, y_train, X_valid, y_valid, model, history):
     y_pred = model.predict(X_valid)
     print("range of real z values: [%f, %f]" % (min(y_valid), max(y_valid)))
 
-    error = [0]*len(y_pred)
+    error = [0] * len(y_pred)
     for j in range(len(y_pred)):
-        error[j] = (y_pred[j] - y_valid[j])**2
+        error[j] = (y_pred[j] - y_valid[j]) ** 2
 
-    print ("Maximum square error: %.4f , minimum square error: %.4f" % (max(error), min(error)))
+    print("Maximum square error: %.4f , minimum square error: %.4f" % (max(error), min(error)))
     print("Metric on validation_set: %.4f (%.4f) MSE (STD)" % (mean(error), std(error)))
-    '''
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    ax.plot_surface(X=X_valid[:10, 0], Y=X_valid[:10, 1], Z=y_pred[:10].reshape(1, 10), cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-    plt.show()
-    '''
+
+    plot_surface(X_valid[:, 0], X_valid[:, 1], y_pred[:, 0])
 
 
 def evaluate_with_keras_pipeline(X_valid, y_valid):
